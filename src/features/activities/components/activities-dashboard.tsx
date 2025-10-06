9b0e02ff9aa6ca121e2b8c4897d305e910d8ddd8
Daniel <danielsalinas305@gmail.com>
2025-10-06 16:47:02 +0200

    refactor: Minor refactor

--- src/features/activities/components/activities-dashboard.tsx
+++ src/features/activities/components/activities-dashboard.tsx
@@ -4,9 +4,9 @@ import { formatDate } from "../../../utils/date";
 import { useActivities } from "../hooks/use-activities";
 import { getTodayCompletedCount } from "../utils";
 
-import styles from "./activities-dashboard.module.css";
 import { ActivityCard } from "./activity-card";
 import { AddActivityForm } from "./add-activity-form";
+import styles from "./activities-dashboard.module.css";
 
 export const ActivitiesDashboard: React.FC = () => {
   const { state } = useActivities();
